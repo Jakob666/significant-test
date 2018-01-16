7cbd6bd34ee4da592814e0e990e88896625774be
jakob <826850754@qq.com>
2018-01-16 18:01:53 +0800

    final version of bootstrap

--- bootstrapTest.py
+++ bootstrapTest.py
@@ -16,6 +16,9 @@ Description:
 ==========================
 @update: 加了更详尽的注释，因为实际项目中出现易混淆点
 @version: 1.3
+==========================
+@update: 对bootstrap的均值、方差的计算做了较大程度上的变化
+@version: 2.0
 '''
 import numpy as np
 # from matplotlib import pyplot as plt
@@ -26,24 +29,26 @@ import warnings
 
 class Boostrap_test:
 
-    def __init__(self, Confidence=0.95, time=1000):
+    def __init__(self, Confidence=0.95, time=1000, side="two-side"):
         '''
         :param Confidence: 置信区间
         :param time: boostrap抽样的次数
+        :param side: 默认是two-side的双尾检验，如果是单尾检验则为"one-side"
         '''
         self.conf = Confidence
         self.times = time
+        self.side = side
         self.t_val_list = []
+        #经过self.time次抽样随机抽取到的样本，也就是模拟得到的总体
+        self.x_total_samples = np.array([])
+        self.y_total_samples = np.array([])
 
     def preprocess(self, x_data, y_data):
-        '''求出x、y样本的合并均值z，将x、y样本的值进行相应的替换：
-            xi'= xi - x_mean + z
-            yi'= yi - y_mean + z
+        '''
+        求取x、y两组观测值的 t'值
         :param x_data: 参数类型是列表，由样本组一的样本值组成
         :param y_data: 参数类型是列表，由样本组二的样本值组成
         '''
-        conbimed = x_data + y_data
-        z = np.mean(conbimed)
         #未经处理的x样本组数据均值
         x_mean = np.mean(x_data)
         x_std = np.std(x_data)
@@ -51,49 +56,68 @@ class Boostrap_test:
         y_mean = np.mean(y_data)
         y_std = np.std(y_data)
         #计算 observed数据的t值
-        t_obs = Boostrap_test.calc_t_val(x_mean, y_mean, len(x_data), len(y_data), x_std, y_std)
-        #x样本组数据预处理
-        x_data = list(map(lambda i: i - x_mean + z, x_data))
-        # x_mean = np.mean(x_data)                                          ##################################
-        #y样本组数据预处理                                                     # 在这两处求取处理后x、y样本组        #
-        y_data = list(map(lambda i: i - y_mean + z, y_data))                # 的均值是无用的，因为均值都是        #
-        # y_mean = np.mean(y_data)                                          # z。之前的处理是将x、y融为一组       #
-                                                                            # ，z是两组样本共同的均值            #
-        return x_data, y_data, t_obs, x_mean, y_mean                        ##################################
+        t_obs = Boostrap_test.calc_t_val(x_mean, y_mean, len(x_data), len(y_data), x_std, y_std, self.side)
+        print(t_obs)
+        return t_obs
 
     def boostrap(self, x_data, y_data, t_obs):
         '''
-        模拟boostrap抽样，每次抽样 size个样本
+        模拟boostrap抽样，每次抽样 size个样本（有放回抽样）
+        求出x、y样本抽样后形成的新的样本的合并均值z，将x、y新样本的值进行相应的替换：
+            xi'= xi - x_mean + z
+            yi'= yi - y_mean + z
         :param x_data: 参数类型是列表，由样本组一的样本值组成
         :param y_data: 参数类型是列表，由样本组二的样本值组成
         :param t_obs: 是一个数值，是preprocess方法中计算出来的两个样本组原始值的 t检验值
         :return:
         '''
+        # z是两组样本中抽取数据共同的均值
+        conbimed = x_data + y_data
+        z = np.mean(conbimed)
+        x_mean = np.mean(x_data)
+        y_mean = np.mean(y_data)
+
         x_size = len(x_data)
         y_size = len(y_data)
         for t in range(self.times):
             x_samples = np.random.choice(x_data, x_size)  #choice方法可以模拟又放回的抽取，size的值可以大于len(data)
             y_samples = np.random.choice(y_data, y_size)
 
+            self.x_total_samples = np.append(self.x_total_samples, x_samples)
+            self.y_total_samples = np.append(self.y_total_samples, y_samples)
+            # x样本组抽取的样本数据预处理
+            x_samples = list(map(lambda i: i - x_mean + z, x_samples))
             x_samples_mean = np.mean(x_samples)
-            x_samples_std = np.std(x_samples)
+            # y样本组抽取的样本数据预处理
+            y_samples  = list(map(lambda i: i - y_mean + z, y_samples))
             y_samples_mean = np.mean(y_samples)
+            x_samples_std = np.std(x_samples)
             y_samples_std = np.std(y_samples)
 
-            t_val = Boostrap_test.calc_t_val(x_samples_mean, y_samples_mean, x_size, y_size, x_samples_std, y_samples_std)
+            t_val = Boostrap_test.calc_t_val(x_samples_mean, y_samples_mean, x_size, y_size, x_samples_std, y_samples_std, self.side)
             self.t_val_list.append(t_val)
+        # print(self.t_val_list)
         p_val = self.calc_p_val(t_obs, self.t_val_list)
 
-        p_ref = 1.0 - self.conf
+        #定义双尾检验时 p为 α/2；单尾检验时 p为α
+        if self.side == "one-side":
+            p_ref = 1 - self.conf
+        elif self.side == "two-side":
+            p_ref = (1 - self.conf)/2
         if p_val < p_ref:
             res = "Significant difference"
         else:
             res = "Insignificant difference"
+        #求取x和y样本组经过self.time次抽取后形成的总体的均值和标准差
+        x_total_mean = np.mean(self.x_total_samples)
+        x_total_std = np.std(self.x_total_samples)
+        y_total_mean = np.mean(self.y_total_samples)
+        y_total_std = np.std(self.y_total_samples)
 
-        return res, p_val, p_ref
+        return res, p_val, p_ref, x_total_mean, y_total_mean, x_total_std, y_total_std
 
     @staticmethod
-    def calc_t_val(x_mean, y_mean, x_size, y_size, x_std, y_std):
+    def calc_t_val(x_mean, y_mean, x_size, y_size, x_std, y_std, side):
         '''计算 bootstrap抽样样本的 t值，此处两组样本的t值使用的是不具备方差齐性时的 t'检验公式
         :param x_mean: 经过预处理后样本组一的均值
         :param y_mean: 经过预处理后样本组二的均值
@@ -102,7 +126,13 @@ class Boostrap_test:
         :param x_std: 经过预处理后样本组一的标准差
         :param y_std: 经过预处理后样本组二的标准差
         '''
-        numerator = np.abs(x_mean - y_mean)
+        if side == "one-side":
+            numerator = x_mean - y_mean
+        elif side == "two-side":
+            numerator = np.abs(x_mean - y_mean)
+        else:
+            print("invalid input")
+            exit()
         denominator = np.sqrt(np.square(x_std)/x_size + np.square(y_std)/y_size)
         t_val = numerator/denominator
         return t_val
@@ -114,9 +144,9 @@ class Boostrap_test:
         '''
         count = 0.0
         for t in range(self.times):
-            if t_obs < t_boot_list[t]:
+            if t_obs > t_boot_list[t]:
                 count += 1.0
-        p_val = count/self.times
+        p_val = count/self.times  #self.time次中有 count次拒绝H0后发生第一类错误的概率小于α
         return p_val
 
     def main(self, x_data, y_data):
@@ -126,10 +156,11 @@ class Boostrap_test:
         :return res是最终的检验结果 "Significant difference"或是"Insignificant difference"；
                 p_val是发生第一类错误的概率； p_ref是α值，即 1-self.conf；
                 x_mean是样本组x的均值； y_mean是样本组y的均值；
+                x_std是样本组x的标准差；y_std是样本组y的标准差。
         '''
-        x_data, y_data, t_obs, x_mean, y_mean = self.preprocess(x_data, y_data)
-        res, p_val, p_ref = self.boostrap(x_data, y_data, t_obs)
-        return res, p_val, p_ref, x_mean, y_mean
+        t_obs = self.preprocess(x_data, y_data)
+        res, p_val, p_ref, x_mean, y_mean, x_std, y_std = self.boostrap(x_data, y_data, t_obs)
+        return res, p_val, p_ref, x_mean, y_mean, x_std, y_std
 
 
 if __name__ == "__main__":
